a78e0e4beb5896540064fbf60daeb6c8bbd33773
aymanhab <ahabib@stanford.edu>
2023-03-16 01:30:08 -0700

    Fit model upon loading, add ambient light

--- src/components/pages/ModelViewPage.tsx
+++ src/components/pages/ModelViewPage.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useEffect } from 'react'
-import { useGLTF, OrbitControls, TrackballControls, GizmoHelper, GizmoViewport } from '@react-three/drei'
+import { useGLTF, OrbitControls, TrackballControls, GizmoHelper, GizmoViewport, Bounds } from '@react-three/drei'
 import BottomBar from './BottomBar';
 import LeftDrawer from './LeftDrawer';
 
@@ -17,8 +17,11 @@ const ModelViewPage = () => {
       <Canvas style={{ width: "100vw", height: "80vh" }} 
               camera={{ position: [1500, 2000, 1000], fov: 50, far: 10000}}>
         <color attach="background" args={['#151518']} />
-        <directionalLight position={[10, 10, 0]} intensity={1.5} />
-        <DefaultModel />
+        <ambientLight intensity={0.3} />
+        <directionalLight position={[10, 10, 0]} />
+        <Bounds fit clip>
+          <DefaultModel />
+        </Bounds>
         <TrackballControls />
         <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
           <GizmoViewport labelColor="white" axisHeadScale={1} />
