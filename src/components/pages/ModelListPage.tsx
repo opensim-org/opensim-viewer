75d7c8bb0d7f172f0fedee4d53ac483ca2ab128e
aymanhab <ahabib@stanford.edu>
2023-03-21 13:01:26 -0700

    ModelList Page now shows a grid of models

--- src/components/pages/ModelListPage.tsx
+++ src/components/pages/ModelListPage.tsx
@@ -1,7 +1,30 @@
-const ModelListPage = () => {
-    return (
-        <h1>This is the model list page!</h1>
-    );
-}
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
 
-export default ModelListPage;
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const models= [ "Arm26.osim", "Pendulum.osim", "leg39.osim", "gait2354.osim", "gait2392.osim", "Rajagopal.osim"];
+
+export default function ModelListPage() {
+  return (
+      <>
+          <Typography variant="h3" style={{ marginTop: 100 }} > Model Gallery </Typography>
+          <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              {Array.from(Array(6)).map((_, index) => (
+                  <Grid xs={2} sm={4} md={4} key={index}>
+                      <Item>{models[index]}</Item>
+                  </Grid>
+              ))}
+          </Grid>
+      </>
+  );
+}
