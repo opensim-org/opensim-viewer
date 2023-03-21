a4e8b68f6af94fca890860c4f0dda19337d6cdd5
aymanhab <ahabib@stanford.edu>
2023-03-21 12:42:38 -0700

    Add grid layout for Model Gallery/List page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -3,9 +3,7 @@ import { Container, Typography } from "@mui/material";
 const HomePage = () => {
     return (
         <Container>
-        <Typography variant="h2" style={{marginTop: 100}}>OpenSim Viewer</Typography>
-        <video width="601" height="325" src="videos/Home1-Fast1080p30.mp4">
-        </video>
+        <Typography variant="h2" style={{marginTop: 100}}>OpenSim Online Viewer</Typography>
         </Container>
     );
 }
