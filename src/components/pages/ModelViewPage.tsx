4d327059218f430973a2d7176217f65b8e099ad2
aymanhab <ahabib@stanford.edu>
2023-03-28 13:59:59 -0700

    Separate OpenSimModel component

--- src/components/pages/ModelViewPage.tsx
+++ src/components/pages/ModelViewPage.tsx
@@ -1,43 +1,10 @@
-import { Canvas, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { useTheme } from '@mui/material'
-import { useGLTF, GizmoHelper, GizmoViewport, Bounds, Environment } from '@react-three/drei'
+import { GizmoHelper, GizmoViewport, Bounds, Environment } from '@react-three/drei'
 import BottomBar from './BottomBar';
 import SettingsDrawer from './SettingsDrawer';
 import OpenSimControl from './OpenSimControl';
-import { Vector3 } from 'three/src/math/Vector3';
-
-function OpenSimModel() {
-  
-   const {
-    gl, // WebGL renderer
-    camera
-  } = useThree();
-
-
-  window.addEventListener("keyup", (event) => {
-    if (event.code==='KeyP'){ // P for print screen
-      const link = document.createElement('a')
-      link.setAttribute('download', 'viewer_snapshot.png')
-      link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'))
-      link.click()
-      event.preventDefault();
-    };
-    if (event.code==='KeyI'){ // I for camera zoom in
-      let v = new Vector3();
-      camera.position.lerp(v.set(1, 0, 0), 0.025);
-    };
-    if (event.code==='KeyO'){ // O for camera zoom out
-      let v = new Vector3();
-      camera.position.lerp(v.set(1, 0, 0), -0.025);
-    }    
-  });
-  
-  // useGLTF suspends the component, it literally stops processing
-  const { scene } = useGLTF('/builtin/leg39_nomusc.gltf' )
-  // By the time we're here the model is guaranteed to be available
-  return <primitive object={scene} />
-}
-
+import { OpenSimModel } from './OpenSimModel';
 
 const ModelViewPage = () => {
   const theme = useTheme();
