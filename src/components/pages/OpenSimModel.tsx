0914dbba269924e19550617a72cf57b791efead6
aymanhab <ahabib@stanford.edu>
2023-04-12 15:38:32 -0700

    Minor refactoring to remove window listener

--- src/components/pages/OpenSimModel.tsx
+++ src/components/pages/OpenSimModel.tsx
@@ -3,6 +3,11 @@ import { useGLTF } from '@react-three/drei';
 import { Vector3 } from 'three';
 import { useMemo } from 'react';
 
+/**
+ * 
+ * OpenSimModel is placed inside the Canvas and so has access to renderer camera and gl-context
+ * hence, we add the methods to handle buttons here even though it feels illogical
+ */
 export function OpenSimModel() {
 
   const {
@@ -10,24 +15,22 @@ export function OpenSimModel() {
     camera
   } = useThree();
 
-
-  window.addEventListener("keyup", (event) => {
-    if (event.code === 'KeyP') { // P for print screen
-      const link = document.createElement('a');
-      link.setAttribute('download', 'viewer_snapshot.png');
-      link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'));
-      link.click();
-      event.preventDefault();
-    };
-    if (event.code === 'KeyI') { // I for camera zoom in
-      let v = new Vector3();
+  void function zoomin () {
+    let v = new Vector3();
       camera.position.lerp(v.set(1, 0, 0), 0.025);
-    };
-    if (event.code === 'KeyO') { // O for camera zoom out
-      let v = new Vector3();
-      camera.position.lerp(v.set(1, 0, 0), -0.025);
-    }
-  });
+  }
+
+  void function zoomOut () {
+    let v = new Vector3();
+    camera.position.lerp(v.set(1, 0, 0), -0.025); 
+  }
+
+  void function snapshot () {
+    const link = document.createElement('a');
+    link.setAttribute('download', 'viewer_snapshot.png');
+    link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'));
+    link.click();
+  }
 
   // useGLTF suspends the component, it literally stops processing
   const { scene } = useGLTF('/builtin/leg39_nomusc.gltf');
