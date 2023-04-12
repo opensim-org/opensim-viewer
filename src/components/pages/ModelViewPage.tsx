d4f6587f4f2ca5891c89b8e2fe246afd89264815
aymanhab <ahabib@stanford.edu>
2023-04-12 10:45:57 -0700

    Use Loader fallback to show progress

--- src/components/pages/ModelViewPage.tsx
+++ src/components/pages/ModelViewPage.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber'
 import { useTheme } from '@mui/material'
-import { GizmoHelper, GizmoViewport, Bounds, Environment} from '@react-three/drei'
+import { GizmoHelper, GizmoViewport, Bounds, Environment, Loader} from '@react-three/drei'
 import BottomBar from './BottomBar';
 import SettingsDrawer from './SettingsDrawer';
 import OpenSimControl from './OpenSimControl';
@@ -8,11 +8,13 @@ import { OpenSimModel } from './OpenSimModel';
 import { Suspense } from 'react';
 
 const ModelViewPage = () => {
+
+  
   const theme = useTheme();
   console.log(theme.palette.mode);
   return (
     <div id="canvas-container">
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loader />}>
       <Canvas gl={{ preserveDrawingBuffer: true }} shadows="soft" 
           style={{ width: "100vw", height: "85vh" }}
           camera={{ position: [1500, 2000, 1000], fov: 75, far: 10000}}>
@@ -21,7 +23,7 @@ const ModelViewPage = () => {
         <Bounds fit clip>
             <OpenSimModel />
         </Bounds>
-        <Environment preset='city' />
+        <Environment preset='city' blur={0.8} />
         <GizmoHelper alignment="bottom-right" margin={[100, 100]}>
           <GizmoViewport labelColor="white" axisHeadScale={1} />
         </GizmoHelper>
