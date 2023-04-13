0f7ce2230139f940edf37be5a9934bcd5ea4e59e
aymanhab <ahabib@stanford.edu>
2023-04-13 00:20:27 -0700

    Move zoom in/out handling to OpenSimControl

--- src/components/pages/OpenSimControl.tsx
+++ src/components/pages/OpenSimControl.tsx
@@ -1,10 +1,36 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from "@react-three/drei";
 import { observer } from "mobx-react";
-import viewerState from '../../state/ViewerState'
+import viewerState from "../../state/ViewerState";
+import { useFrame, useThree } from "@react-three/fiber";
+import { Vector3 } from "three";
 
 function OpenSimControl() {
-    //console.log(viewerState.rotating);
-    return <OrbitControls autoRotate autoRotateSpeed={viewerState.rotating?2:0.0} makeDefault />
+  const {
+     camera,
+  } = useThree();
+
+  let v = new Vector3();
+  useFrame(() => {
+    if (viewerState.zooming) {
+      if (viewerState.zoomFactor > 1.0) {
+        camera.position.lerp(v.set(1, 0, 0), 0.02);
+        camera.updateProjectionMatrix();
+
+      } else if (viewerState.zoomFactor < -1.0) {
+        camera.position.lerp(v.set(1, 0, 0), -0.02);
+        camera.updateProjectionMatrix();
+      }
+      viewerState.zooming = false;
+    }
+  });
+  //console.log(viewerState.rotating);
+  return (
+    <OrbitControls
+      autoRotate
+      autoRotateSpeed={viewerState.rotating ? 2 : 0.0}
+      makeDefault
+    />
+  );
 }
 
 export default observer(OpenSimControl);
--- src/components/pages/OpenSimModel.tsx
+++ src/components/pages/OpenSimModel.tsx
@@ -1,4 +1,4 @@
-import { useThree, useFrame } from '@react-three/fiber';
+import { useThree } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import { Vector3 } from 'three';
 import { useMemo } from 'react';
@@ -31,21 +31,6 @@ const OpenSimModel: React.FC<OpenSimModelProps> = ({ curentModelPath }) => {
       link.click();
   }
 
-  useFrame(()=>{
-    if (viewerState.zooming){
-      if (viewerState.zoomFactor > 1.0){
-      let v = new Vector3();
-        camera.position.lerp(v.set(1, 0, 0), 0.02);
-        camera.updateProjectionMatrix();
-      }
-      else if (viewerState.zoomFactor < -1.0){
-      let v = new Vector3();
-        camera.position.lerp(v.set(1, 0, 0), -0.02);
-        camera.updateProjectionMatrix();
-    }
-      viewerState.zooming = false;
-    }
-  });
   // useGLTF suspends the component, it literally stops processing
   const { scene } = useGLTF(curentModelPath);
   useMemo(() => scene.traverse(obj => {
