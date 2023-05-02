bd7e7e57995999307b41864c4b1d608fe9d3a7a1
aymanhab <ahabib@stanford.edu>
2023-05-02 14:44:26 -0700

    Populate Animation object, channels, targets, no data yet

--- src/backend/convertTrc2Gltf.py
+++ src/backend/convertTrc2Gltf.py
@@ -5,7 +5,7 @@ Created on Mon Apr 24 14:37:54 2023
 """
 
 import opensim as osim
-import pygltflib as pygltf
+from pygltflib import *
 import numpy as np
 import json
 from pathlib import Path
@@ -44,10 +44,10 @@ def convertTrc2Gltf(trcFilePath, shape) :
     else:
         print("File has no Units specifications, meters assumed.")
     firstDataFrame = table.getRowAtIndex(0)
-    gltf = pygltf.GLTF2().load('basicShapes.gltf')
+    gltf = GLTF2().load('basicShapes.gltf')
 
     # create node for the marker mesh, refer to it from all marker nodes
-    topNode = pygltf.Node()
+    topNode = Node()
     topNode.name = 'MarkerData'
     gltf.nodes.clear()
     gltf.nodes.append(topNode)
@@ -56,9 +56,10 @@ def convertTrc2Gltf(trcFilePath, shape) :
     sceneNodes = default_scene.nodes
     sceneNodes.clear()
     sceneNodes.append(0) # MarkerData topNode
+    # Create nodes for the experimental markers, 1 node per marker
     for markerIndex in range(numMarkers):
       # Create node for the marker
-      nextMarkerNode = pygltf.Node()
+      nextMarkerNode = Node()
       nextMarkerNode.name = table.getColumnLabel(markerIndex)
       # 0 cube, 1 sphere, 2 brick
       desiredShape = shape2Mesh.get(shape)
@@ -70,7 +71,9 @@ def convertTrc2Gltf(trcFilePath, shape) :
 
       # extras are place holder for application specific properties
       # for now we'll pass opensimType, may add layers, as needs arise....
-      opensim_extras = {"opensimType": "experimental-marker"}
+      opensim_extras = {"opensimType": "ExperimentalMarker", 
+                        "layer": "data", 
+                        "name": table.getColumnLabel(markerIndex)}
       nextMarkerNode.extras = opensim_extras
       translation = firstDataFrame.getElt(0, markerIndex).to_numpy()
 
@@ -83,7 +86,91 @@ def convertTrc2Gltf(trcFilePath, shape) :
       gltf.nodes.append(nextMarkerNode)
       topNode.children.append(markerIndex+1)
 
+    convertTableDataToGltfAnimation(gltf, table)
     return gltf
+
+def convertTableDataToGltfAnimation(gltfTop, timeSeriesTableVec3) :
+  "Take marker data and convert into animations in gltf format" 
+  # Create an animations node under top level
+  animation = Animation()
+  gltfTop.animations.append(animation)
+  # create two nodes one for samplers, the other for channel per Marker
+  numMarkers = timeSeriesTableVec3.getNumColumns()
+  # create buffer, bufferview and  accessor for timeframes
+  timeColumn = timeSeriesTableVec3.getIndependentColumn()
+  addTimeStampsAccessor(gltfTop, timeColumn)
+  for markerIndex in range(numMarkers): #do one marker only to start
+    sampler = AnimationSampler()
+    # sampler.input = ??
+    sampler.interpolation = ANIM_LINEAR
+    sampler.output = markerIndex+1  #node number for markerIndex
+    animation.samplers.append(sampler)
+
+    channel = AnimationChannel()
+    channel.sampler = markerIndex
+    target = AnimationChannelTarget()
+    target.node = markerIndex
+    target.path = "translation"
+    channel.target = target
+    animation.channels.append(channel)
+    # create accessor for data
+    addDataAccessor(gltfTop, timeSeriesTableVec3.getDependentColumnAtIndex(markerIndex))
+    # bind accessor to target
+
+def addTimeStampsAccessor(gltf, timesColumn):
+  "Add buffer, bufferview and accessor for timestamps"
+  # precompute offsets as cross references use index from file
+  startBufferNumberOffset = gltf.buffers.count()
+  startBufferViewNumberOffset = gltf.bufferViews.count()
+  startAccessorsOffset = gltf.accessors.count()
+
+  timeBuffer = Buffer()
+  timeBufferView = BufferView()
+  timeAccessor = Accessor()
+  timeAccessor.bufferView = startBufferViewNumberOffset
+  timeAccessor.byteOffset = 0
+  timeAccessor.componentType = FLOAT
+  timeAccessor.count = 1
+  timeAccessor.type = SCALAR
+  gltf.accessors.append(timeAccessor)
+
+  gltf.buffers.append(timeBuffer)
+  gltf.bufferViews.append(timeBufferView)
+  # Above this line is boiler plate bookkeeping regardless of actual data
+  # the code below depends on actual data
+  timeBuffer.byteLength = 4 * len(timesColumn)
+  timeBuffer.uri =""
+  timeAccessor.max = [max(timesColumn)]
+  timeAccessor.min = [min(timesColumn)]
+
+
+def addDataAccessor(gltf, dataColumn):
+  "Add buffer, bufferview and accessor for marker data column"
+  # precompute offsets as cross references use index from file
+  startBufferNumberOffset = gltf.buffers.count()
+  startBufferViewNumberOffset = gltf.bufferViews.count()
+  startAccessorsOffset = gltf.accessors.count()
+
+  markerDataBuffer = Buffer()
+  markerDataBufferView = BufferView()
+  markerDataAccessor = Accessor()
+  markerDataAccessor.bufferView = startBufferViewNumberOffset
+  markerDataAccessor.byteOffset = 0
+  markerDataAccessor.componentType = FLOAT
+  markerDataAccessor.count = 3
+  markerDataAccessor.type = VEC3
+  gltf.accessors.append(markerDataAccessor)
+
+  gltf.buffers.append(markerDataBuffer)
+  gltf.bufferViews.append(markerDataBufferView)
+  # Above this line is boiler plate bookkeeping regardless of actual data
+  # the code below depends on actual data
+  markerDataBuffer.byteLength = 4 * len(dataColumn)
+  markerDataBuffer.uri =""
+  markerDataAccessor.max = [max(dataColumn)]
+  markerDataAccessor.min = [min(dataColumn)]
+
+
 
 def main():
     import argparse
@@ -109,7 +196,8 @@ def main():
         outfile = infile.replace('.trc', '.gltf')
     else:
         outfile = args.output
-    convertTrc2Gltf(infile, args.shape).save(outfile)
-
+    
+    resultGltf = convertTrc2Gltf(infile, args.shape)
+    resultGltf.save(outfile)
 
 main()
