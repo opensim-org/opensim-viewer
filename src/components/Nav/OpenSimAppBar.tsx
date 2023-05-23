d7f5bdfc31524f6cff59c9c448a870fcd49d57a3
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2023-05-22 17:01:37 -0700

    Data persistence when moving to home, models and viewer

--- src/components/Nav/OpenSimAppBar.tsx
+++ src/components/Nav/OpenSimAppBar.tsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
 import logo_dark from './logo-dark.svg';
 import logo from './logo.svg';
@@ -14,6 +13,7 @@ import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import viewerState from '../../state/ViewerState';
+import { NavLink } from 'react-router-dom'
 
 interface OpenSimAppBarProps {
     dark: boolean
@@ -24,7 +24,7 @@ const OpenSimAppBar: React.FC<OpenSimAppBarProps> = ({ dark }) => {
 return (
   <AppBar position="static">
     <Toolbar variant="dense" color="inherit">
-      <Link href="/">
+      <Link component={NavLink} to="/">
         <Box
           component="img"
           sx={{ height: 60 }}
@@ -32,22 +32,24 @@ return (
           src={dark ? logo_dark : logo}
         />
       </Link>
-      <Button href="/viewer" sx={{ marginLeft: "auto" }}>
+      
+      <Link component={NavLink} to="/viewer" sx={{ marginLeft: "auto"}}>
         <Typography variant="button" color="secondary">
           Viewer
         </Typography>
-      </Button>
-      <Button href="/models">
+      </Link>
+
+      <Link component={NavLink} to="/models" sx={{ marginLeft: "1em"}}>
         <Typography variant="button" color="secondary">
           Models
         </Typography>
-      </Button>
+      </Link>
+
       <IconButton
         sx={{ ml: 1 }}
         onClick={() => {
           viewerState.setDark(!viewerState.dark);
         }}
-        color="inherit"
       >
         {viewerState.dark ? <Brightness4Icon /> : <Brightness7Icon />}
       </IconButton>
