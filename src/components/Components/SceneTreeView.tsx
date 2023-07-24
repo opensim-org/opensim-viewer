c13b09cec71a7791575fc0cb5936789125b759b4
aymanhab <ahabib@stanford.edu>
2023-07-24 16:05:41 -0700

    Separate out SceneTreeItem for customization and selection handling

--- src/components/Components/SceneTreeView.tsx
+++ src/components/Components/SceneTreeView.tsx
@@ -1,13 +1,46 @@
 import TreeView from '@mui/lab/TreeView'; 
-import TreeItem from "@mui/lab/TreeItem";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 import { modelUIState } from "../../state/ModelUIState";
+import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
+import CustomTreeItem from './SceneTreeItem';
+
+function MinusSquare(props: SvgIconProps) {
+    return (
+      <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+        {/* tslint:disable-next-line: max-line-length */}
+        <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
+      </SvgIcon>
+    );
+  }
+
+  function PlusSquare(props: SvgIconProps) {
+    return (
+      <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
+        {/* tslint:disable-next-line: max-line-length */}
+        <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
+      </SvgIcon>
+    );
+  }
+
+  function CloseSquare(props: SvgIconProps) {
+    return (
+      <SvgIcon
+        className="close"
+        fontSize="inherit"
+        style={{ width: 14, height: 14 }}
+        {...props}
+      >
+        {/* tslint:disable-next-line: max-line-length */}
+        <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
+      </SvgIcon>
+    );
+  }
+
 const SceneTreeView  = ()  => {
     function createTreeItemForNode(anode: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, index: number) {
         let computeId = (3+index);
-        return <TreeItem nodeId={computeId.toString()} label={anode.name} key={computeId}/>
+        let threeObj = anode.threeObj;
+        return <CustomTreeItem nodeId={computeId.toString()} label={anode.name} key={computeId} />
     }
     const sTree = modelUIState.sceneTree
     const meshesNode = sTree?.rootNode?.children[0]
@@ -16,14 +49,15 @@ const SceneTreeView  = ()  => {
         <TreeView
             aria-label="file system navigator"
             sx={{ flexGrow: 1, maxWidth: 450, overflowY: 'auto' }}
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-        >   
-            <TreeItem nodeId="1" label={sTree?.rootNode?.name} key={1} >
-                <TreeItem nodeId="2" label={sTree?.rootNode?.children[0].name} key={2}>
+            defaultCollapseIcon={<MinusSquare />}
+            defaultExpandIcon={<PlusSquare />}
+            defaultEndIcon={<CloseSquare />}
+        >
+            <CustomTreeItem  nodeId="1" label={sTree?.rootNode?.name} key={1} >
+                <CustomTreeItem  nodeId="2" label={sTree?.rootNode?.children[0].name} key={2}>
                     {meshesArray?.map(createTreeItemForNode)}
-                </TreeItem>
-            </TreeItem>
+                </CustomTreeItem>
+            </CustomTreeItem>
         </TreeView>
     );
 }
