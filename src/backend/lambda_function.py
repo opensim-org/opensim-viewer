fc7b248ce0c4c5715edcefeee9985ccf32873b1d
aymanhab <ahabib@stanford.edu>
2023-10-17 13:55:55 -0700

    Update Dockerfile to use opensim-python:4.3 as base, refactor function to do conversion

--- src/backend/lambda_function.py
+++ src/backend/lambda_function.py
@@ -1,43 +1,40 @@
+from __future__ import print_function
 import json
 import opensim as osim
 import pygltflib
 from osimConverters import *
+# import boto3
+import time, urllib
+import json
+"""Code snippet for copying the objects from AWS source S3 bucket to target S3 bucket as soon as objects uploaded on source S3 bucket
+"""
 
+print ("Initializing..")
 
+# s3 = boto3.client('s3')
 def handler(event, context):
     print(event)
     print(context)
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+    target_bucket = 'opensim-viewer-public-download'
+    copy_source = {'Bucket': source_bucket, 'Key': object_key}
+    
+    print ("Source bucket : ", source_bucket)
+    print ("Target bucket : ", target_bucket)
+    print ("Log Stream name: ", context.log_stream_name)
+    print ("Log Group name: ", context.log_group_name)
+    print ("Request ID: ", context.aws_request_id)
+    print ("Mem. limits(MB): ", context.memory_limit_in_mb)
+    # Construct urls for input, output files and call convertUserFileToGLTF
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
     }
 
-def convertUserFileToGLTF(filePathOrURL):
-        filename, file_extension = os.path.splitext(filePathOrURL.name)
-        if (file_extension==".trc"):
-        #invoke converter on trc file, then generate url from path
-            gltfJson = convertTrc2Gltf(filePathOrURL, 'sphere')
-            outfile = filePathOrURL.replace('.trc', '.gltf')
-            # gltfJson.save(outfile)
-        elif (file_extension==".c3d"):
-            gltfJson = convertC3D2Gltf(filePathOrURL, 'sphere')
-            outfile = filePathOrURL.replace('.c3d', '.gltf')
-            # gltfJson.save(outfile)
-        elif (file_extension==".mot"):
-            gltfJson = convertMotForce2Gltf(filePathOrURL, 'arrow')
-            outfile = filePathOrURL.replace('.mot', '.gltf')
-            # gltfJson.save(outfile)
-        elif (file_extension==".osim"):
-            gltfJson = convertOsim2Gltf(filePathOrURL, 'Geometry')
-            outfile = filePathOrURL.replace('.osim', '.gltf')
-            # gltfJson.save(outfile)
-        elif (file_extension==".osimz"):
-            gltfJson = convertOsimZip2Gltf(filePathOrURL)
-            outfile = filePathOrURL.replace('.osimz', '.gltf')
-            # gltfJson.save(outfile)
-         #elif (file_extension==".gltf"):
-        #    generatedFile = filePathOrURL
-        return gltfJson
+def convertUserFileToGLTF(inFilePathOrURL, outFilePathOrURL):
+    gltfJson = convertNativeFileToGLTF(inFilePathOrURL)
+    gltfJson.save(outFilePathOrURL)
 
 
 def whatever():
--- src/backend/osimConverters/__init__.py
+++ src/backend/osimConverters/__init__.py
@@ -4,3 +4,23 @@ from .convertC3D2Gltf import *
 from .convertMotForce2Gltf import *
 from .convertOsim2Gltf import *
 from .convertOsimZip2Gltf import *
+
+def convertNativeFileToGLTF(inFilePathOrURL):
+        filename, file_extension = os.path.splitext(inFilePathOrURL.name)
+        if (file_extension==".trc"):
+        #invoke converter on trc file, then generate url from path
+            gltfJson = convertTrc2Gltf(inFilePathOrURL, 'sphere')
+        elif (file_extension==".c3d"):
+            gltfJson = convertC3D2Gltf(inFilePathOrURL, 'sphere')
+             # gltfJson.save(outfile)
+        elif (file_extension==".mot"):
+            gltfJson = convertMotForce2Gltf(inFilePathOrURL, 'arrow')
+             # gltfJson.save(outfile)
+        elif (file_extension==".osim"):
+            gltfJson = convertOsim2Gltf(inFilePathOrURL, 'Geometry')
+            # gltfJson.save(outfile)
+        elif (file_extension==".osimz"):
+            gltfJson = convertOsimZip2Gltf(inFilePathOrURL)
+
+        return gltfJson
+        
