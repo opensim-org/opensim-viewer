8d71d599da13104f75ccfdf32eb098feb226085e
aymanhab <ahabib@stanford.edu>
2023-10-18 22:59:25 -0700

    Remove debugging lines and dead code

--- src/backend/lambda_function.py
+++ src/backend/lambda_function.py
@@ -17,15 +17,8 @@ def handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     object_key = event['Records'][0]['s3']['object']['key']
     target_bucket = 'opensim-viewer-public-download'
-    copy_source = {'Bucket': source_bucket, 'Key': object_key}
     
-    print ("Source bucket : ", source_bucket)
-    print ("Target bucket : ", target_bucket)
-    print ("key : ", object_key)
-    print ("Log Stream name: ", context.log_stream_name)
-    print ("Log Group name: ", context.log_group_name)
-    print ("Request ID: ", context.aws_request_id)
-    print ("Mem. limits(MB): ", context.memory_limit_in_mb)
+    osim.Logger.setLevelString('Off')
     # Construct urls for input, output files and call convertUserFileToGLTF
     try:
         print ("Using waiter to waiting for object to persist through s3 service")
@@ -40,15 +33,15 @@ def handler(event, context):
         print("Gltf file saved")
         destinationFileName = Path(object_key).with_suffix('.gltf')
         strDestinationFileName = str(destinationFileName)
-        print("DestinationFile string", strDestinationFileName)
+        # print("DestinationFile string", strDestinationFileName)
         s3.upload_file(destinationFile, target_bucket, strDestinationFileName)
-        print("File upload launched")
+        # print("File upload launched")
         result_url = s3.generate_presigned_url(
             'get_object',
             Params={'Bucket': target_bucket,
                     'Key': strDestinationFileName },
             ExpiresIn=3600)
-        print("Gltf file uploaded to ", result_url)
+        # print("Gltf file uploaded to ", result_url)
         waiter = s3.get_waiter('object_exists')
         waiter.wait(Bucket=target_bucket, Key=strDestinationFileName) 
         print("File upload finished, url returned", result_url)       # s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
@@ -67,45 +60,7 @@ def convertUserFileToGLTF(inFilePathOrURL, outFilePathOrURL):
     gltfJson.save(outFilePathOrURL)
 
 
-def whatever():
-    print('whatever has been successfully called')
-
 def main():
     print('main was called')
 
 main()
-# from __future__ import print_function
-# import boto3
-# import time, urllib
-# import json
-# """Code snippet for copying the objects from AWS source S3 bucket to target S3 bucket as soon as objects uploaded on source S3 bucket
-# """
-
-# print ("Initializing..")
-
-# s3 = boto3.client('s3')
-
-# def lambda_handler(event, context):
-#     # TODO implement
-#     source_bucket = event['Records'][0]['s3']['bucket']['name']
-#     object_key = event['Records'][0]['s3']['object']['key']
-#     target_bucket = 'opensim-viewer-public-download'
-#     copy_source = {'Bucket': source_bucket, 'Key': object_key}
-    
-#     print ("Source bucket : ", source_bucket)
-#     print ("Target bucket : ", target_bucket)
-#     print ("Log Stream name: ", context.log_stream_name)
-#     print ("Log Group name: ", context.log_group_name)
-#     print ("Request ID: ", context.aws_request_id)
-#     print ("Mem. limits(MB): ", context.memory_limit_in_mb)
-    
-#     try:
-#         print ("Using waiter to waiting for object to persist through s3 service")
-#         waiter = s3.get_waiter('object_exists')
-#         waiter.wait(Bucket=source_bucket, Key=object_key)
-#         print ("converting file ")
-#         s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
-#         return response['ContentType']
-#     except Exception as err:
-#         print ("Error -"+str(err))
-#         return e
