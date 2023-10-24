1b2f820e83367f76d855f3b50ca95141f7dfbf44
aymanhab <ahabib@stanford.edu>
2023-10-24 12:21:38 -0700

    minor cleanup and loosen parsing of file extension

--- src/backend/lambda_function.py
+++ src/backend/lambda_function.py
@@ -17,43 +17,41 @@ def handler(event, context):
     source_bucket = event['Records'][0]['s3']['bucket']['name']
     object_key = event['Records'][0]['s3']['object']['key']
     target_bucket = 'opensim-viewer-public-download'
-    
+    file_name = '/tmp/' + object_key.split('/')[-1]
+    print("file_name", file_name)
     osim.Logger.setLevelString('Off')
     # Construct urls for input, output files and call convertUserFileToGLTF
     try:
-        print ("Using waiter to waiting for object to persist through s3 service")
-        waiter = s3.get_waiter('object_exists')
-        waiter.wait(Bucket=source_bucket, Key=object_key)
         workFolder = "/tmp/"
-        s3.download_file(source_bucket, object_key, workFolder+object_key)
-        gltfJson = convertNativeFileToGLTF(workFolder+object_key)
-        destinationFile = Path(workFolder+object_key).with_suffix('.gltf')
+        print("Attempting to download")
+        s3.download_file(source_bucket, object_key, file_name)
+        print ("/tmp dirs:", os.listdir("/tmp"))
+        print ("calling convertNativeFileToGLTF with:", file_name)
+        gltfJson = convertNativeFileToGLTF(file_name)
+        destinationFile = Path(file_name).with_suffix('.gltf')
         print ("output file", destinationFile)
         gltfJson.save(destinationFile)
         print("Gltf file saved")
-        destinationFileName = Path(object_key).with_suffix('.gltf')
-        strDestinationFileName = str(destinationFileName)
+        destinationFileName = Path(file_name).with_suffix('.gltf')
+        strDestinationFileName = str(destinationFileName).split('/')[-1]
         # print("DestinationFile string", strDestinationFileName)
         s3.upload_file(destinationFile, target_bucket, strDestinationFileName)
         # print("File upload launched")
-        result_url = s3.generate_presigned_url(
+        response = s3.generate_presigned_url(
             'get_object',
             Params={'Bucket': target_bucket,
                     'Key': strDestinationFileName },
-            ExpiresIn=3600)
+            ExpiresIn=3600,
+            HttpMethod='GET')
         # print("Gltf file uploaded to ", result_url)
-        waiter = s3.get_waiter('object_exists')
-        waiter.wait(Bucket=target_bucket, Key=strDestinationFileName) 
-        print("File upload finished, url returned", result_url)       # s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
+        print("File upload finished, url returned", response)       # s3.copy_object(Bucket=target_bucket, Key=object_key, CopySource=copy_source)
         return {
         'statusCode': 200,
-        'body': json.dumps('Success'),
-        'result_url': result_url
+        'body': json.dumps(response, default=str)
         }
     except Exception as err:
         print ("Error -"+str(err))
         return err
-    
 
 def convertUserFileToGLTF(inFilePathOrURL, outFilePathOrURL):
     gltfJson = convertNativeFileToGLTF(inFilePathOrURL)
--- src/backend/osimConverters/__init__.py
+++ src/backend/osimConverters/__init__.py
@@ -7,19 +7,19 @@ from .convertOsimZip2Gltf import *
 
 def convertNativeFileToGLTF(inFilePathOrURL):
         filename, file_extension = os.path.splitext(inFilePathOrURL)
-        if (file_extension==".trc"):
+        if (file_extension==".trc" or ".trc." in inFilePathOrURL):
         #invoke converter on trc file, then generate url from path
             gltfJson = convertTrc2Gltf(inFilePathOrURL, 'sphere')
-        elif (file_extension==".c3d"):
+        elif (file_extension==".c3d" or ".c3d." in inFilePathOrURL):
             gltfJson = convertC3D2Gltf(inFilePathOrURL, 'sphere')
              # gltfJson.save(outfile)
-        elif (file_extension==".mot"):
+        elif (file_extension==".mot" or ".mot." in inFilePathOrURL):
             gltfJson = convertMotForce2Gltf(inFilePathOrURL, 'arrow')
              # gltfJson.save(outfile)
-        elif (file_extension==".osim"):
+        elif (file_extension==".osim" or ".osim." in inFilePathOrURL):
             gltfJson = convertOsim2Gltf(inFilePathOrURL, 'Geometry')
             # gltfJson.save(outfile)
-        elif (file_extension==".osimz"):
+        elif (file_extension==".osimz" or ".osimz." in inFilePathOrURL):
             gltfJson = convertOsimZip2Gltf(inFilePathOrURL)
 
         return gltfJson
