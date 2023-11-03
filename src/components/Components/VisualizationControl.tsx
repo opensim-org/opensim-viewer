c719453b743b5600e7b91a69ac3e8e159a581bc0
aymanhab <ahabib@stanford.edu>
2023-11-03 11:27:49 -0700

    Fix build errors

--- src/components/Components/VisualizationControl.tsx
+++ src/components/Components/VisualizationControl.tsx
@@ -37,46 +37,6 @@ return (
                     onChange={()=>{curState.toggleLayerVisibility(4); setCameraLayerMask(curState.cameraLayersMask)}}/>
         </FormGroup>
       </Container>
-      <Container disableGutters>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 150 }}>
-            <InputLabel id="simple-select-standard-label">Animations</InputLabel>
-            <Select 
-            labelId="simple-select-standard-label"
-            label={t('visualizationControl.animate')}
-            value={selectedAnim}
-            onChange={handleAnimationChange}
-            >
-            {props.animationList.map(anim => (
-            <MenuItem key={anim.name} value={anim.name}>
-              {anim.name}
-            </MenuItem>
-          ))}
-        </Select>
-        </FormControl>
-          <Stack direction="row" color="primary">
-          <IconButton 
-                color="primary"
-                value={'Animation'} 
-                onClick={togglePlayAnimation}>
-                {play?<PauseCircleTwoToneIcon/>:<PlayCircleTwoToneIcon/>}
-            </IconButton>
-            <FormControl size="small">
-                <InputLabel id="simple-select-standard-label2">Speed</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={speed.toString()}
-                    label={t('visualizationControl.speed')}
-                    onChange={handleSpeedChange}
-                >
-                    <MenuItem value={0.25}>0.25</MenuItem>
-                    <MenuItem value={0.5}>0.5</MenuItem>
-                    <MenuItem value={1.0}>1.0</MenuItem>
-                    <MenuItem value={2.0}>2.0</MenuItem>
-                </Select>
-            </FormControl>
-            </Stack>
-        </Container>
     </>
     )
 }
