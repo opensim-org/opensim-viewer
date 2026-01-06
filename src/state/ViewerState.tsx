221c0849d1bc08e875ba06be9b3af1b367db1955
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2026-01-06 11:22:18 -0800

    Removed lower quality options.

--- src/state/ViewerState.tsx
+++ src/state/ViewerState.tsx
@@ -141,7 +141,7 @@ export class ViewerState {
         this.recordedVideoAspectRatio = "16:9"
         this.isRecordingVideo = isRecordingVideo
         this.isProcessingVideo = isProcessingVideo
-        this.videoRecorderBaseDimension = 720
+        this.videoRecorderBaseDimension = 1440
 
         this.videoRecorderPreserveAspectRatio = true
         this.user_uuid = ''
