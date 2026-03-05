65227b294291cc70a5260a3d731b4a66aecc85c5
Alberto Casas Ortiz <albertocasasortiz@gmail.com>
2026-03-05 14:41:47 -0800

    Added userSelect: 'none' to logo.

--- src/components/Components/OpenSimLogo.tsx
+++ src/components/Components/OpenSimLogo.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const OpenSimHtmlLogo: React.FC = () => {
 
 return (
-    <div style={{ position: 'absolute', bottom: 20, maxWidth: '250px', maxHeight: '250px', left: 30, zIndex: 1 }}>
+    <div style={{ position: 'absolute', bottom: 20, maxWidth: '250px', maxHeight: '250px', left: 30, zIndex: 1, userSelect: 'none', }}>
         <img src="/assets/opensimLogo23.png" width="40%" height="40%" alt="OpenSimLogo"/>
     </div>
   );
